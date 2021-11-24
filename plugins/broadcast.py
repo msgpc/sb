3310ab67014515fb9b81024cf86a461a2f874223
Kaipullavadivelu <86786455+Kaipullavadivelu@users.noreply.github.com>
2021-11-24 18:36:51 +0530

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -2,7 +2,7 @@
 from pyrogram import Client, filters
 import datetime
 import time
-import db
+from database.users_chats_db import DATABASE_URI
 from info import ADMINS
 from utils import broadcast_messages
 import asyncio
