1cde14a39903a5b542ebcf7680bf53f3c9fd05a9
Kaipullavadivelu <86786455+Kaipullavadivelu@users.noreply.github.com>
2021-12-12 11:47:04 +0530

    Update info.py

--- info.py
+++ info.py
@@ -16,7 +16,7 @@ USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
 
 BROADCAST_CHANNEL = int(os.environ.get("BROADCAST_CHANNEL", ""))
 BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST", True))
-DB_URL = os.environ.get("DATABASE_URI", "")
+DB_URL = os.environ.get("DATABASE_1", "")
 ADMIN_ID = set(int(x) for x in os.environ.get("ADMIN_ID", "").split())
 
 # Admins, Channels & Users
